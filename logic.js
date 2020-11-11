f482ab6b285e0036cec1fc2aa690f85b249730ce
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-11-10 21:43:54 -0600

    Adding changes to the header

--- logic.js
+++ logic.js
@@ -1,8 +1,28 @@
 document.getElementById("buttonstart").onclick = function() {myFunction()};
+const userName = document.getElementById("username");
 
 function myFunction() {
   event.preventDefault();
    var name = prompt("What's your name?");
    alert("Hello " + name);
+   localStorage.setItem("user", name);
    location.href = "application.html";
+
 }
+
+function get_localStorage(){
+    let name = localStorage.getItem("user");
+    alert(name);
+    userName.innerHTML=  name; 
+}
+
+// function getUserName() {
+ //   event.preventDefault();
+  //  userName.text("new text");
+  //  alert("hello");
+ // }
+  
+//   window.getUserName;
+
+
+
